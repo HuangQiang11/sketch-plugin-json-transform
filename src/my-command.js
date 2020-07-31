ff5ec416925c145beaf2a00f136ef22a58816027
HuangQiang11 <15914302575@163.com>
2020-07-31 19:00:21 +0800

    保存文件优化

--- src/my-command.js
+++ src/my-command.js
@@ -12,97 +12,118 @@ export default function() {
   const desingTokensPages = pages.filter(page => page.selected === true)
   const desingTokensPage = desingTokensPages[0]
   if (!desingTokensPage) {
-    UI.alert("请选择目标页面", "未选中页面");
+    UI.alert("请选择目标页面", "未选中页面")
     return
   }
 
   var selectPage = desingTokensPage
 
   const artboards = selectPage.layers.filter(layer => layer.selected === true)
-  var artboard = artboards[0]
-  if (!artboard) {
-     artboard = selectPage.layers[0]
-  }
-  if (!artboard) {
-    UI.alert("选中内容为空", "未选中页面");
-    return
-  }
-  var jsonContent = {
-    "canvas_w": artboard.frame.width,
-    "canvas_h": artboard.frame.height,
-    "bg_color": artboard.background.color
-  }
-  var poster_shapes = []
-  var poster_image = []
-  var poster_text = []
-  var poster_other = []
-  var export_layers = []
 
-  artboard.layers.forEach(function (layer, i) {
-    var item = {
-        "frame": layer.frame,
-        "name": layer.name,
-        "level": i,
-        "type_name": layer.type,
-        "locked":layer.locked
+  if (artboards.length == 0) {
+    UI.alert("请选中画板", "未选中画板")
+    return;
+  }
+
+  var dataSource = []
+  let jsonKey = 'jsonKey'
+  let layerKey = 'layerKey'
+  let nameKey = 'nameKey'
+  for (let index in artboards) {
+    let artboard = artboards[index]
+    var jsonContent = {
+      "canvas_w": artboard.frame.width,
+      "canvas_h": artboard.frame.height,
+      "bg_color": artboard.background.color
     }
-    if (layer.type == 'Text') {
-      item["style"] = layer.style
-      item["layer_type"] = 1
-      poster_text.push(item)
-    }else if (layer.type == 'Image' || layer.type == 'Group') {
-      item["layer_type"] = 2
-      item["type_name"] = 'Image'
-      poster_image.push(item)
-    }else if (layer.type == 'ShapePath') {
-      item["svg"] = layer.getSVGPath()
-      item["layer_type"] = 3
-      poster_shapes.push(item)
-    }else {
-      item["layer_type"] = 4
-      poster_other.push(item)
-    }
-    export_layers.push(layer)
+    var poster_shapes = []
+    var poster_image = []
+    var poster_text = []
+    var poster_other = []
+    var export_layers = []
 
-  })
-  jsonContent["layer_shapes"] = poster_shapes
-  jsonContent["layer_images"] = poster_image
-  jsonContent["layer_texts"] = poster_text
-  jsonContent["layer_other"] = poster_other
 
-  let jsonStr = JSON.stringify(jsonContent)
-  let defaultFileName = "config.json"
-  let contents = jsonStr
-  let save = NSSavePanel.savePanel();
-  save.setNameFieldStringValue(defaultFileName);
-  save.setAllowsOtherFileTypes(false);
-  save.setExtensionHidden(false);
-
-  if (save.runModal()) {
-    let file = NSString.stringWithString(contents);
-    let path = save.URL().path();
-
-    var imagePath = path
-    imagePath = imagePath.replace("/"+defaultFileName, "")
-     const exportOptions = {
-          formats: 'png',
-          scales: '2x',
-          output: imagePath,
-          overwriting: true,
-          trimmed: false
+    artboard.layers.forEach(function (layer, i) {
+        var item = {
+          "frame": layer.frame,
+          "name": layer.name,
+          "level": i,
+          "type_name": layer.type,
+          "locked":layer.locked
         }
-    for (let index in export_layers) {
-        var export_item = export_layers[index]
-        if (export_item.name != 'cut_position') {
-          export_item.name = export_item.name
-          sketch.export(export_item, exportOptions)
+        if (layer.type == 'Text') {
+          item["style"] = layer.style
+          item["layer_type"] = 1
+          poster_text.push(item)
+        }else if (layer.type == 'Image' || layer.type == 'Group') {
+          item["layer_type"] = 2
+          item["type_name"] = 'Image'
+          poster_image.push(item)
+        }else if (layer.type == 'ShapePath') {
+          item["svg"] = layer.getSVGPath()
+          item["layer_type"] = 3
+          poster_shapes.push(item)
+        }else {
+          item["layer_type"] = 4
+          poster_other.push(item)
         }
+        export_layers.push(layer)
+
+    })
+    jsonContent["layer_shapes"] = poster_shapes
+    jsonContent["layer_images"] = poster_image
+    jsonContent["layer_texts"] = poster_text
+    jsonContent["layer_other"] = poster_other
+
+    let jsonStr = JSON.stringify(jsonContent)
+    let itemData = {
+      jsonKey: jsonStr,
+      layerKey: export_layers,
+      nameKey: artboard.name
     }
-
-
-    file.writeToFile_atomically_encoding_error(path, true, NSUTF8StringEncoding, null);
-    UI.alert("保存成功", path);
+    dataSource.push(itemData)
   }
+
+  const open = NSOpenPanel.openPanel();
+  open.canChooseFiles = false
+  open.canChooseDirectories = true
+  open.canCreateDirectories = true
+  if (open.runModal()) {
+    var path = open.URL().path();
+    let fileManager = NSFileManager.defaultManager()
+    for(let index in dataSource) {
+      let itemData = dataSource[index]
+      let jsonStr = itemData[jsonKey]
+      let layerArr = itemData[layerKey]
+      let name = itemData[nameKey]
+      let dirPath = path + "/" + name
+      let b = fileManager.fileExistsAtPath_isDirectory(dirPath, null)
+      if (!b) {
+        fileManager.createDirectoryAtPath_withIntermediateDirectories_attributes_error(dirPath, true, null, null)
+      }
+      let configPath = dirPath + '/config.json'
+      let file = NSString.stringWithString(jsonStr);
+      file.writeToFile_atomically_encoding_error(configPath, true, NSUTF8StringEncoding, null);
+
+      var imagePath = dirPath
+       const exportOptions = {
+            formats: 'png',
+            scales: '2x',
+            output: imagePath,
+            overwriting: true,
+            trimmed: false
+        }
+      for (let index in layerArr) {
+          var export_item = layerArr[index]
+          if (export_item.name != 'cut_position') {
+            export_item.name = export_item.name
+            sketch.export(export_item, exportOptions)
+          }
+      }
+    }
+    UI.alert("保存成功", path)
+  }
+
 
 
   // console.log(JSON.stringify(jsonContent))
